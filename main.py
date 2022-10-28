e1c96d179d83f63da621b68aedd2ed37d7c8e8f9
wgetmintian <112361582+wgetmintian@users.noreply.github.com>
2022-10-28 10:18:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -420,7 +420,9 @@ body = ''
 
 print('- loading...')
 if URL_BASE != '' and '/' not in URL_BASE:
-    driver = uc.Chrome(use_subprocess=True, version_main=105)
+    options = uc.ChromeOptions()
+    options.headless = True 
+    driver = uc.Chrome(use_subprocess=True, driver_executable_path="/root/.local/share/undetected_chromedriver/chromedriver", options=options)
     driver.set_window_size(785, 627)
     driver.set_page_load_timeout(30)
     set_driver(driver)
